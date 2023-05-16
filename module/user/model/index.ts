d745ca0c655080a40141afc3aabbd2c7747929f0
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-05-16 09:48:52 +0700

    chore : update user schema

---
+++ module/user/model/index.ts
@@ -0,0 +1,65 @@
+import mongoose, { Schema } from 'mongoose'
+
+// Define interface for User document
+export interface IUser {
+  name: string
+  username: string
+  email: string
+  password: string
+  role: 'admin' | 'user'
+  is_verified?: boolean
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+// Define user schema
+const userSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    username: {
+      type: String,
+      required: [true, 'username is required'],
+      unique: true,
+      validate: {
+        validator: async function (username: string) {
+          const user = await User.findOne({ username })
+          return !user
+        },
+        message: 'Username already exists.',
+      },
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      validate: {
+        validator: async function (email: string) {
+          const user = await User.findOne({ email })
+          return !user
+        },
+        message: 'Email already exists.',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    role: {
+      type: String,
+      required: [true, 'Role is required'],
+      default: 'user',
+    },
+    is_verified: {
+      type: Boolean,
+      required: [true, 'Password is required'],
+      default: false,
+    },
+  },
+  { timestamps: true }
+)
+const User = mongoose.model<IUser>('User', userSchema)
+
+export default User
