0f4916da7e14532ede5da4fb04a3c8693c8d1984
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-05-16 07:19:31 +0700

    chore : config env

--- config/database.ts
+++ config/database.ts
@@ -1,9 +1,25 @@
 // const mongoose = require('mongoose')
 import mongoose, { ConnectOptions } from 'mongoose'
 import asyncHandler from 'express-async-handler'
+
 import { MONGODB_URI } from '../utils'
 
-const dbConnect = asyncHandler(async () => {
+// const dbConnect = asyncHandler(async (req, res, next) => {
+//   try {
+//     const conn = await mongoose.connect(MONGODB_URI, {
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true,
+//     } as ConnectOptions)
+
+//     console.log(`MongoDB Connected : ${conn.connection.host}`)
+//   } catch (err: any) {
+//     console.error(err.message)
+//     process.exit(1)
+//   }
+// })
+
+// export default dbConnect
+const dbConnect = async () => {
   try {
     const conn = await mongoose.connect(MONGODB_URI, {
       useUnifiedTopology: true,
@@ -11,11 +27,11 @@ const dbConnect = asyncHandler(async () => {
     } as ConnectOptions)
 
     console.log(`MongoDB Connected : ${conn.connection.host}`)
-  } catch (err:any) {
+  } catch (err: any) {
     console.error(err.message)
     process.exit(1)
   }
-})
+}
 
 export default dbConnect
 // module.exports = dbConnect
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
-export const {
-  MONGODB_URI : MONGODB_URI = '',
-} = process.env
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const { MONGODB_URI: MONGODB_URI = '', PORT = 5000 } = process.env
