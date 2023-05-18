ebfd9be218f0a91c3eff1c5ef541fbc3ea836cfa
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-05-18 11:26:30 +0700

    feat : add auth module

--- index.ts
+++ index.ts
@@ -1,17 +1,41 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
-import { PORT } from './utils'
-import dbConnect from './config/database'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
 
+import { NODE_ENV, PORT } from './utils'
+import dbConnect from './config/database'
 dotenv.config()
+
+/* Routes */
+import seederRoutes from './module/seeder/routes'
+import authRoutes from './module/auth/routes'
 
 const app: Express = express()
 dbConnect()
 
+if (process.env.NODE_ENV?.trim() === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(cookieParser())
+app.use(express.json())
+
+/* Routes */
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server is running ggggggggggggg')
+  res.send('Express + TypeScript Server is running')
 })
 
+// Auth routes
+app.use('/api/v1/auth', authRoutes)
+
+// Seeder route
+if (NODE_ENV?.trim() === 'development') {
+  app.use('/api/v1/seeder', seederRoutes)
+}
+
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+  console.log(
+    `⚡️[server]: Server is running  ${process.env.NODE_ENV} mode on http://localhost:${PORT}`
+  )
 })
--- utils/index.ts
+++ utils/index.ts
@@ -2,4 +2,13 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const { MONGODB_URI: MONGODB_URI = '', PORT = 5000 } = process.env
+export const {
+  MONGODB_URI: MONGODB_URI = '',
+  NODE_ENV,
+  PORT = 5000,
+  JWT_SECRET = 'secret-token',
+  REFRESH_TOKEN_SECRET = 'refresh-token',
+  JWT_EXPIRES_IN,
+  JWT_COOKIE_EXPIRES_IN,
+  JWT_COOKIE_EXPIRES_IN_MS,
+} = process.env
