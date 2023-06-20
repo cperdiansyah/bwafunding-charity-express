ba2295b4fe3420f1a12cffd95ab96e1c54576568
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-06-20 21:32:12 +0700

    feat : add upload file from url charity

--- src/module/media/routes/index.ts
+++ src/module/media/routes/index.ts
@@ -1,9 +1,9 @@
 import express from 'express'
-import { verifyAnonymousToken } from '../../../middleware/verifyToken.js'
+import { verifyToken } from '../../../middleware/verifyToken.js'
 import { postTempMedia } from '../controller/index.js'
 const router = express.Router()
 
 // Post Media
-router.route('/upload').post([verifyAnonymousToken], postTempMedia)
+router.route('/upload').post([verifyToken], postTempMedia)
 
 export default router
