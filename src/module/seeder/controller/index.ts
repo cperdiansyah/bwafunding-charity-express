e9540309840f4cbcffad0e4d4efe9cbcc9c558d4
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-07-22 21:47:26 +0700

    chore : update seeder

--- src/module/seeder/controller/index.ts
+++ src/module/seeder/controller/index.ts
@@ -13,12 +13,12 @@ import banners from '../../../data/banner.js'
 import { IBanner } from '../../banner/model/banner.interface.js'
 import Banner from '../../banner/model/index.js'
 import PaymentCampaign from '../../payment/charity/model/index.js'
+import Approval from '../../approval/model/index.js'
+import CharityFundHistory from '../../charity/model/fund_history.js'
 
 export const importData = async (req: Request, res: Response) => {
   try {
-    await User.deleteMany({})
-    await Charity.deleteMany({})
-    await Banner.deleteMany({})
+    await destroy()
 
     const createdUsers = await User.insertMany(users)
     const adminUser = createdUsers[0]._id
@@ -56,10 +56,7 @@ export const importData = async (req: Request, res: Response) => {
 
 export const destroyData = async (req: Request, res: Response) => {
   try {
-    await User.deleteMany({})
-    await Charity.deleteMany({})
-    await Banner.deleteMany({})
-    await PaymentCampaign.deleteMany({})
+    await destroy()
     res.status(200).json({
       status: 'success',
       message: 'Data Destroyed',
@@ -74,4 +71,13 @@ export const destroyData = async (req: Request, res: Response) => {
     })
     // process.exit()
   }
+}
+
+const destroy = async () => {
+  await User.deleteMany({})
+  await Charity.deleteMany({})
+  await Banner.deleteMany({})
+  await Approval.deleteMany({})
+  await CharityFundHistory.deleteMany({})
+  await PaymentCampaign.deleteMany({})
 }
