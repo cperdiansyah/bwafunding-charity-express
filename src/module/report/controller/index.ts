a670a041a3bcb39a472b237a764f7ead896bfe1c
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-08-12 17:31:52 +0700

    chore : report endpoint campaign done

--- src/module/report/controller/index.ts
+++ src/module/report/controller/index.ts
@@ -1,11 +1,9 @@
 import { Request, Response, NextFunction } from 'express'
-import mongoose from 'mongoose'
-import axios from 'axios'
 import fs from 'fs'
+import ejs from 'ejs'
 
 import path from 'path'
 import { __dirname } from '../../../utils/index.js'
-import multer from 'multer'
 
 import puppeteer from 'puppeteer'
 
@@ -30,6 +28,8 @@ export const generateCampaignReport = async (req: Request, res: Response) => {
       })
     }
 
+    /* Get Content Data */
+
     const campaign: ICharity | null = await Charity.findById(idCampaign)
       .populate({
         path: 'author',
@@ -44,8 +44,6 @@ export const generateCampaignReport = async (req: Request, res: Response) => {
         },
       })
     }
-   
-
     const getPaymentData = await api.get(
       `${SERVICE.Transaction}/charity/${idCampaign}`,
       {
@@ -56,19 +54,71 @@ export const generateCampaignReport = async (req: Request, res: Response) => {
           status: 'settlement',
         },
         headers: {
-          ...req.headers
-        }
+          ...req.headers,
+        },
       }
     )
-
     const dataPayment = getPaymentData.data
 
-    const pdfUrl = ''
+    /* Prepare uploaded document */
+    const relativeUploadLocation = 'storage/report'
+    const uploadFolder = path.resolve(__dirname, relativeUploadLocation)
+
+    if (!fs.existsSync(uploadFolder)) {
+      fs.mkdirSync(uploadFolder, { recursive: true })
+    }
+
+    /* Generate PDF Document*/
+
+    // Create a browser instance
+    const browser = await puppeteer.launch({
+      headless: true,
+    })
+
+    // Create a new page
+    const page = await browser.newPage()
+
+    //Get HTML content from HTML file
+    // const html = fs.readFileSync('index.html', 'utf-8')
+    // console.log(html)
+
+    // const htmlFilePath = path.join(__dirname, 'templates', 'report.html')
+    // const contentHTML = fs.readFileSync(htmlFilePath, 'utf-8')
+    const renderedHTML = await ejs.renderFile(
+      path.join(__dirname, 'templates', 'report.ejs'),
+      { campaign: campaign, paymentData: dataPayment }
+    )
+
+    await page.setContent(renderedHTML, { waitUntil: 'domcontentloaded' })
+
+    // To reflect CSS used for screens instead of print
+    await page.emulateMediaType('screen')
+
+    // Downlaod the PDF
+    const pdfBuffer = await page.pdf({
+      // path: 'result.pdf',
+      margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+      printBackground: true,
+      format: 'A4',
+    })
+
+    const pdfName = `report-${Date.now()}.pdf`
+
+    // Write the downloaded file to the uploads folder
+    const filePath = `${relativeUploadLocation}/${pdfName}`
+
+    fs.writeFileSync(filePath, pdfBuffer)
+
+    const pdfUrl = `${req.protocol}://${req.get('host')}/${filePath}`
+    // Close the browser instance
+    await browser.close()
+
     return res.status(200).json({
       data: {
         campaign: campaign,
         paymentData: dataPayment,
       },
+      url: pdfUrl,
     })
   } catch (error) {
     return errorHandler(error, res)
